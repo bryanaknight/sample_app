1386787fbdf162391976fdc3d2f99f0771b085a3
Bryana Knight <brknig11@gmail.com>
2013-12-02 20:42:48 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   # get "pages/home"
   # get "pages/contact"
   # get "pages/about"
   # get "pages/help"
   root :to => 'pages#home'
+  get '/signup', to: 'users#new'
   get '/contact', to: 'pages#contact'
   get '/about', to: 'pages#about'
   get '/help', to: 'pages#help'
+
 
  
 
